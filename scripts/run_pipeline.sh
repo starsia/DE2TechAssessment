ee38b195fbb2415c2c3a321edce082107a35a372
Sia Zhong Qing <starsia93@gmail.com>
2026-06-28 19:56:52 +0800

    Use venv for script

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -6,6 +6,8 @@ PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 
 cd "$PROJECT_ROOT"
 
+source "$PROJECT_ROOT/myenv/bin/activate"
+
 export PYTHONPATH="$PROJECT_ROOT/src"
 
 python -m data_pipelines.pipeline

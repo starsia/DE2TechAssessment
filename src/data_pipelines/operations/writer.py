7739e94a011cbd52a1cfa847fae34975afdcf162
Sia Zhong Qing <starsia93@gmail.com>
2026-06-28 19:15:40 +0800

    Add load function as writer.py

--- src/data_pipelines/operations/writer.py
+++ src/data_pipelines/operations/writer.py
@@ -1,2 +1,55 @@
-def write(successful_df, unsuccessful_df):
-    return
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+import pandas as pd
+
+ROOT = Path(__file__).resolve().parents[3]
+
+OUTPUT = ROOT / "data" / "output"
+
+
+def write(
+    successful_df: pd.DataFrame,
+    unsuccessful_df: pd.DataFrame,
+    input_files: list[Path],
+) -> Path:
+    """
+    Creates a batch output directory.
+
+    output/
+        YYYYMMDD_HHMMSS/
+            input/
+            successful.csv
+            unsuccessful.csv
+    """
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    batch = OUTPUT / timestamp
+
+    input_dir = batch / "input"
+
+    input_dir.mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+
+    successful_df.to_csv(
+        batch / "successful.csv",
+        index=False,
+    )
+
+    unsuccessful_df.to_csv(
+        batch / "unsuccessful.csv",
+        index=False,
+    )
+
+    # move processed source files
+    for file in input_files:
+        shutil.move(
+            str(file),
+            str(input_dir / file.name),
+        )
+
+    return batch
